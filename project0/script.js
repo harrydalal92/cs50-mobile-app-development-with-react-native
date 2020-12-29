9f462fef1d9aaf38a739f3a47b0c0a0a32f44197
harrydalal92 <60006727+harrydalal92@users.noreply.github.com>
2020-12-30 02:26:20 +0530

    Update script.js

--- project0/script.js
+++ project0/script.js
@@ -9,6 +9,52 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let totalTodosCount = 0
+let totalUncheckedTodos = 0
+let id = 0
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  let textOfNewTodo = prompt("Title of you New Todo: ")
+
+  if (textOfNewTodo.length > 0) {
+  	totalTodosCount++
+    totalUncheckedTodos++
+    itemCountSpan.innerHTML = totalTodosCount
+    uncheckedCountSpan.innerHTML = totalUncheckedTodos
+    createTodo(textOfNewTodo)
+  }
+}
+
+function createTodo(text) {
+	let todoListItem = document.createElement('li')
+  id++
+	todoListItem.id = "todo"+id
+	todoListItem.innerHTML = '<input type="checkbox" id="'+id+'" onClick="checkTodo('+id+')" /> <span>'+text+'</span> <button onClick="deleteTodo('+id+')">Delete</button>'
+	list.appendChild(todoListItem)
+}
+
+function checkTodo(id) {
+  var currCheckBox = document.getElementById(id)
+  if (currCheckBox.checked) {
+    totalUncheckedTodos = totalUncheckedTodos - 1
+  } else {
+    totalUncheckedTodos = totalUncheckedTodos + 1
+  }
+  uncheckedCountSpan.innerHTML = totalUncheckedTodos
+}
+
+function deleteTodo(id) {
+
+  var currTodoListItem = document.getElementById("todo"+id)
+  var currCheckBox = document.getElementById(id)
+
+  totalTodosCount = totalTodosCount -1
+  itemCountSpan.innerHTML = totalTodosCount
+
+  if (!currCheckBox.checked) {
+    totalUncheckedTodos = totalUncheckedTodos - 1
+    uncheckedCountSpan.innerHTML = totalUncheckedTodos
+  }
+
+  list.removeChild(currTodoListItem)
 }
