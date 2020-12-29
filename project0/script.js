d27e876d165ef05c891ebd4d552f979574d90a2f
harrydalal92 <60006727+harrydalal92@users.noreply.github.com>
2020-12-30 02:29:22 +0530

    Completed project0

--- project0/script.js
+++ project0/script.js
@@ -5,6 +5,36 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+let totalTodosCount = 0
+let totalUncheckedTodos = 0
+let id = 0
+
+function newTodo() {
+  let textOfNewTodo = prompt("Title of you New Todo: ")
+
+  if (textOfNewTodo.length > 0) {
+  	totalTodosCount++
+    totalUncheckedTodos++
+    itemCountSpan.innerHTML = totalTodosCount
+    uncheckedCountSpan.innerHTML = totalUncheckedTodos
+    
+    createTodo(textOfNewTodo)
+  }
+}
+
+function createTodo(text) {
+	let todoListItem = document.createElement('li')
+  id++
+	todoListItem.id = "todo"+id
+	todoListItem.innerHTML = '<input type="checkbox" id="'+id+'" onClick="checkTodo('+id+')" /> <span>'+text+'</span> <button onClick="deleteTodo('+id+')">Delete</button>'
+	list.appendChild(todoListItem)
+}
+
+function checkTodo(id) {
   var currCheckBox = document.getElementById(id)
   if (currCheckBox.checked) {
     totalUncheckedTodos = totalUncheckedTodos - 1
